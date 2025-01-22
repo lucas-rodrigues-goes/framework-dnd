f787d0348ba85da8105e032017300f0001c3549a
LucasG0es <lucasrodriguesgoes@outlook.com>
2025-01-22 00:08:53 -0300

    Images, Database, Character Sheet
    created an images file to contain default images for screens, and made screens work with it.

---
+++ front_end/javascript/images.js
@@ -0,0 +1,54 @@
+`[r:'`
+
+const condition_images = {
+    special: "asset://38525024141fedd5b4503268f411f587",
+    spell: "asset://8fcbdc20a730e85f62f073323af54f9f",
+    curse: "asset://17a66eb974c4513994a93f7bc40a2ac1",
+    poison: "asset://b53a685d0b14ddd2eb01807f86cd7573",
+};
+
+const class_images = {
+    barbarian: "asset://d963c8b40a27e349e6239dcc3a1cbce2",
+    bard: "asset://17a9f3a727fb50c75bff76d4e8ed6d27",
+    cleric: "asset://039a32067cd1c26bf1e18c423a218f5c",
+    druid: "asset://f03485d26b9728b00fc54c9e4e4b5b4c",
+    fighter: "asset://0775774cade2e68e4a9c4bd5c83ea282",
+        "Battle Master": "asset://e44045582f8f31922e6c0bd793b10077",
+        Champion: "asset://1e718dbea4b8624c664fd346724216d6",
+        "Eldritch Knight": "asset://d616194e11b05843b3553697ae268e05",
+    monk: "asset://8047e1c0694233eb11d5a0f669e61a54",
+    paladin: "asset://f3fcf46aaf9c1a13e33f40c62a91f541",
+    ranger: "asset://90baa66f12545c72476e0cfd3050620f",
+    rogue: "asset://e105c25e25210a5e107d49ef0af2bfb5",
+    sorcerer: "asset://57460f85630f7d84602ff9128eccfef4",
+    warlock: "asset://9663e79e3e5c493352a92ca0ba6acc60",
+    wizard: "asset://feb415509eb88654c71b1fa53d0879f1",
+};
+
+const feature_images = {
+    racial: "asset://3432c572c31826e72dedd81ef4a10c5b",
+    feat: "asset://fc6c201b48e30ba2d9da845a14f92e60",
+    class: "asset://65479d57a2b1ffc116a12a5c731d91a7",
+};
+
+const proficiency_images = {
+    combat: "asset://9394034e19ad1bcefb025a1fd971f57c",
+    skill: "asset://e7fc8f132555db0f9ca8d51ee6ffb1ee",
+    language: "asset://36292f4d1c468032b3090c6fce40751d",
+};
+
+const spell_school_images = {
+    abjuration: "asset://610c5852df8ef040a3c1ba678fbd2e3d",
+    conjuration: "asset://ac65fdab4c0af0eb6e502ea508c27607",
+    divination: "asset://4aad8d5a0358701909aea571612a2485",
+    enchantment: "asset://8d2464a3ae0afde2ce0ec69abd65aa2f",
+    evocation: "asset://79f5407b61a5d7cf13137910bb0cf3fc",
+    illusion: "asset://d0a8db9574e38703b490746c17d91560",
+    necromancy: "asset://4e7caf122839630f94575156850cf067",
+    transmutation: "asset://536f6fd178afa1dc3d79789af1112104",
+};
+
+
+//===================================================================================================//
+
+`']`
