fb14dc118a32cc68bee1be660879e735836eb102
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-03-07 23:03:41 -0300

    Update: Character Sheet

--- back_end/class/entities/Humanoid.js
+++ back_end/class/entities/Humanoid.js
@@ -46,7 +46,20 @@ try {
             return total
         }
 
+        get level_up_experience () { 
+                return {
+                    1: 300, 2: 900, 3: 2700, 4: 6500, 5: 14000, 6: 23000, 7: 34000, 8: 48000,
+                    9: 64000, 10: 85000, 11: 100000, 12: 120000, 13: 140000, 14: 165000, 15: 195000,
+                    16: 225000, 17: 265000, 18: 305000, 19: 355000
+                }[this.level]
+        }
+
         get experience () { return this.#experience }
+        set experience (new_exp) {
+            this.#experience = new_exp
+            this.save()
+        }
+
         get classes () { return this.#classes }
 
         level_up(class_choice, choices = {features: [], skills: []}) {
