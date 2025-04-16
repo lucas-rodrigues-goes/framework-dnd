b0ef77acbbf9ed645afbbfe6fa400e15a136751f
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-04-16 10:24:43 -0300

    Update: Entity (X, Y, visibility)
    Now you can extract from an entity, its X and Y, as well as move it in a chosen direction by a number of cells.
    
    Additionally the new function target_visibility() receives a target token, and returns how much of the token can be seen by the entity.

--- back-end/class/entities/entity.js
+++ back-end/class/entities/entity.js
@@ -8,14 +8,10 @@ try {
 
         #id;
         #token;
-        #portrait;
-        #image;
 
         constructor(id) {
             this.#id = id
             this.#token = MapTool.tokens.getTokenByID(id)
-            this.#portrait = MTScript.evalMacro(`[r:getTokenPortrait("","`+id+`")]`)
-            this.#image = MTScript.evalMacro(`[r:getTokenImage("","`+id+`")]`)
         }
 
 
@@ -23,26 +19,25 @@ try {
         // Getter methods
         //=====================================================================================================
 
+        get id() { return this.#id }
         get token() { return this.#token;}
-        get portrait() { return this.#portrait;}
-        get image() { return this.#image;}
-        
-        get player() {
-            return this.#token.isPC()
-        }
+
+        get portrait() { return MTScript.evalMacro(`[r:getTokenPortrait("","`+this.id+`")]`)}
+        get image() { return MTScript.evalMacro(`[r:getTokenImage("","`+this.id+`")]`)}
+        get x() {return Number(MTScript.evalMacro(`[r:getTokenX(0,"`+this.id+`")]`))}
+        get y() {return Number(MTScript.evalMacro(`[r:getTokenY(0,"`+this.id+`")]`))}
+        get player() {return this.token.isPC()}
 
 
         //=====================================================================================================
         // Setter methods
         //=====================================================================================================
 
-        get id() { return this.#id }
-        get token() { return this.#token;}
-        get portrait() { return this.#portrait;}
-        
+        set x(x) { MTScript.evalMacro(`[r:moveToken(`+x+`, `+this.y+`, 0, "`+this.id+`")]`) }
+        set y(y) { MTScript.evalMacro(`[r:moveToken(`+this.x+`, `+y+`, 0, "`+this.id+`")]`) }
         set player(player) {
-            if (player) { this.#token.setPC() } 
-            else { this.#token.setNPC() }
+            if (player) this.token.setPC()
+            else this.token.setNPC()
         }
 
 
@@ -50,9 +45,32 @@ try {
         // Functions
         //=====================================================================================================
 
-        go_to() { MTScript.evalMacro(`[r:goTo("`+this.#id+`")]`) }
-        select() { MTScript.evalMacro(`[r:selectTokens("`+this.#id+`")]`) }
-        impersonate() { MTScript.evalMacro(`[r:impersonate("`+this.#id+`")]`) }
+        go_to() { MTScript.evalMacro(`[r:goTo("`+this.id+`")]`) }
+        select() { MTScript.evalMacro(`[r:selectTokens("`+this.id+`")]`) }
+        impersonate() { MTScript.evalMacro(`[r:impersonate("`+this.id+`")]`) }
+        target_visibility(target=selected()) {
+            const visible_points = JSON.parse(MTScript.evalMacro(`[r:canSeeToken("`+target.id+`","`+this.id+`")]`))
+            return visible_points.length / 5
+        }
+        move(direction, cells) {
+            cells = Number(cells)
+            if (isNaN(cells)) return
+
+            switch (direction) {
+                case "up":
+                    this.y -= cells
+                    break
+                case "down":
+                    this.y += cells
+                    break
+                case "left":
+                    this.x -= cells
+                    break
+                case "right":
+                    this.x += cells
+                    break
+            }
+        }
         
 
         //=====================================================================================================
