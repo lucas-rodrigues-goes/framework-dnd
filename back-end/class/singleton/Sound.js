cef9c3160a5342d398390795254036783563a7cb
Lucas Góes <lucasrodriguesgoes@outlook.com>
2025-05-23 15:48:19 -0300

    Update: Sound

--- back-end/class/singleton/Sound.js
+++ back-end/class/singleton/Sound.js
@@ -6,38 +6,52 @@ var Sound = class {
     // Stored Attributes
     //-----------------------------------------------------------------------------------------------------
 
-    static #data = MapTool.tokens.getTokenByID("D111E023F771474D9A6567941FB4B350");
+    static #data = MapTool.tokens.getTokenByID("7644598835564E7EA8BC5E323C508749");
 
     //-----------------------------------------------------------------------------------------------------
     // Getters / Setters
     //-----------------------------------------------------------------------------------------------------
 
-    static get currently_playing() {
+    static get currently_streaming() {
         const object = JSON.parse(this.#data.getProperty("object"));
-        return object?.currently_playing || [];
+        return object?.currently_streaming || [];
     }
 
-    static set currently_playing(currently_playing) {
+    static set currently_streaming(currently_streaming) {
         const object = JSON.parse(this.#data.getProperty("object")) || {};
-        object.currently_playing = currently_playing;
+        object.currently_streaming = currently_streaming;
         
         this.#data.setProperty("object", JSON.stringify(object));
     }
 
     //-----------------------------------------------------------------------------------------------------
-    // Methods
+    // Sound Playing/Stopping Methods
     //-----------------------------------------------------------------------------------------------------
 
-    static play (name, {volume = 0.1, loop = false, stream = false} = {}) {
+    static play (name, {volume = 0.1, stream = false} = {}) {
         try {
+            // Play Sound
             MTScript.evalMacro(`
                 [h,macro("playLocal${ stream ? "Stream" : "Clip" }All@lib:JUH.media"): json.append("",
                     "audio:${name}",
-                    ${loop ? -1 : 1},
+                    ${stream ? -1 : 1},
                     ${volume}
                 )]
             `)
-        } catch {}
+
+            // Add stream to array
+            const wasStreaming = this.currently_streaming.includes(name)
+            if (stream) {
+                if (!wasStreaming) this.currently_streaming = [...this.currently_streaming, name]
+            }
+            // If is going to be replayed, but not as a loop remove from streaming list
+            else if (wasStreaming) {
+                this.currently_streaming = this.currently_streaming.filter((elem) => elem != name)
+            }
+        } 
+        catch {
+            console.log(`Error occurred when playing ${name}`, "debug")
+        }
     }
 
     static stop (name) {
@@ -48,7 +62,18 @@ var Sound = class {
                     0
                 )]
             `)
-        } catch {}
+
+            // Remove stream from array
+            this.currently_streaming = this.currently_streaming.filter((elem) => elem != name)
+        } 
+        catch {
+            console.log(`Error occurred when stopping ${name}`, "debug")
+        }
     }
 
+    static stopAll () {
+        for (const sound of this.currently_streaming) {
+            this.stop(sound)
+        }
+    }
 }
