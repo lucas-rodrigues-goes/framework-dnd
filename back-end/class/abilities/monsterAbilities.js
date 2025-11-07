9f335e21485c4c38cb285108d5d047e6a7e3519a
lucas-rodrigues-goes <186176911+lucas-rodrigues-goes@users.noreply.github.com>
2025-11-07 15:56:03 -0300

    feat(monster abilities): attack handling mutliple targets

--- back-end/class/abilities/monsterAbilities.js
+++ back-end/class/abilities/monsterAbilities.js
@@ -11,17 +11,6 @@ var MonsterAbilities = class extends Abilities {
             return { valid: false };
         }
 
-        if (!selected()) {
-            public_log(`${creature.name_color} needs to select a target for this action.`);
-            return { valid: false };
-        }
-
-        // Validate Visibility
-        if (creature.target_visibility() == 0) {
-            public_log(`${creature.name_color} needs to see their target.`);
-            return { valid: false };
-        }
-
         return { valid: true, creature, target, targets };
     }
 
@@ -37,51 +26,81 @@ var MonsterAbilities = class extends Abilities {
     }
 
     static attack(ability) {
-        const target = selected()
 
         // Requirements
-        const { valid, creature } = this.check_action_requirements(ability);
-        if (!valid || !target) return;
+        const { valid, creature, targets } = this.check_action_requirements(ability);
+        if (!valid || !creature) return;
 
-        // Make attack
-        let advantage_weight = 0
-
-        // Range Validation
-        const range_valid = (calculate_distance(creature, target) * 5) <= ability.range
-        if (!range_valid) {
-            console.log(`${creature.name_color} tried to attack ${target.name_color} using their ${weapon?.name || "fists"} but they are out of range.`, "public")
+        // Validate target selection count
+        if (targets.length === 0) {
+            console.log(`${creature.name_color} must select at least 1 target.`, "all");
+            return;
+        }
+        if (targets.length > ability.max_targets) {
+            console.log(`${creature.name_color} can only target up to ${ability.max_targets} creature(s) with ${ability.name}.`, "all");
+            return;
+        }
+        if (targets.length > 3) {
+            console.log(`${creature.name_color} cannot target more than 3 creatures with any attack.`, "all");
+            return;
         }
 
-        // Release Sound
-        Sound.play("swing", 0.1)
-
-        // Calculate Hit
-        const hit_bonus = ability.hit_bonus
-        const hit_result = this.attack_hit_result({hit_bonus, creature, target, advantage_weight})
-
-        // Deal damage
-        const damage_result = (hit_result.success 
-            ? ` dealing ${this.spell_damage(creature, target, hit_result.message, ability.damage)} damage.`
-            : `.`
-        )
-
-        // Make stealth tests and others
-        this.attack_roll_advantage_modifiers({creature, target})
-
-        // Output
-        const attack_result = {
-            success: true,
-            hit_result: hit_result,
-            damage_result: damage_result,
-            message: `${creature.name_color} attacks ${target.name_color} (AC ${target.armor_class}) with ${ability.name} and ${hit_result.message} (${hit_result.dice_roll.text_color})${damage_result}`
+        // Validate adjacency if more than 1 target
+        if (targets.length > 1) {
+            for (const t1 of targets) {
+                let adjacent = false;
+                for (const t2 of targets) {
+                    if (t1.id === t2.id) continue;
+                    if (calculate_distance(t1, t2) <= 1) {
+                        adjacent = true;
+                        break;
+                    }
+                }
+                if (!adjacent) {
+                    console.log(`${creature.name_color} must select adjacent targets for this attack.`, "all");
+                    return;
+                }
+            }
         }
 
-        // Consume resources
-        this.use_resources(ability.resources)
-        Initiative.set_recovery(ability.recovery, creature)
+        // Attack each target
+        let i = 0;
+        for (const target of targets) {
+            // Range Validation
+            const in_range = (calculate_distance(creature, target) * 5) <= ability.range;
+            if (!in_range) {
+                console.log(`${creature.name_color} tried to attack ${target.name_color} using ${ability.name} but they are out of range.`, "all");
+                continue;
+            }
 
-        // Logging
-        public_log(attack_result.message)
+            // Release sound only on first strike
+            if (i === 0) Sound.play("swing", 0.1);
+
+            // Calculate hit
+            const hit_bonus = ability.hit_bonus;
+            const hit_result = this.attack_hit_result({
+                hit_bonus,
+                creature,
+                target,
+                advantage_weight: 0
+            });
+
+            // Damage result
+            const damage_result = hit_result.success
+                ? ` dealing ${this.spell_damage(creature, target, hit_result.message, ability.damage)} damage.`
+                : `.`
+
+            // Output
+            const message =
+                `${creature.name_color} attacks ${target.name_color} (AC ${target.armor_class}) with ${ability.name} and ${hit_result.message} (${hit_result.dice_roll.text_color})${damage_result}`;
+
+            public_log(message);
+            i++;
+        }
+
+        // Consume resources only once
+        this.use_resources(ability.resources);
+        Initiative.set_recovery(ability.recovery, creature);
     }
 
     static special(ability) {
