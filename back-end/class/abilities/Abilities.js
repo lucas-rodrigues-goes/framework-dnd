1b303fe5bd251ccbc80ce694b425c8ced95c5862
lucas-rodrigues-goes <186176911+lucas-rodrigues-goes@users.noreply.github.com>
2025-11-07 14:44:47 -0300

    feat(abilities): sanitization of values

--- back-end/class/abilities/Abilities.js
+++ back-end/class/abilities/Abilities.js
@@ -346,9 +346,9 @@ var Abilities = class {
         let count = 0
         for (const damage of damage_dice) {
             // Calculate Damage
-            const die_size = damage.die_size || 0
-            const die_amount = damage.die_amount * crit_multiplier
-            const damage_bonus = damage.damage_bonus || 0
+            const die_size = Number(damage.die_size) || 0
+            const die_amount = Number(damage.die_amount) * crit_multiplier
+            const damage_bonus = Number(damage.damage_bonus) || Number(damage.bonus_damage) || 0
 
             const damage_to_deal = Math.floor((roll_dice(die_amount, die_size) + damage_bonus) * damage_multiplier)
             count ++
